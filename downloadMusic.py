9239ff27284a12e4d3d3ab525dbeb7d0dcbb008d
Atilla Saadat <atillaabc@gmail.com>
2020-06-11 13:12:45 -0400

    update

--- downloadMusic.py
+++ downloadMusic.py
@@ -1,9 +1,14 @@
 import subprocess
 import os
 from IPython import embed
-playlistLink = 'https://www.youtube.com/playlist?list=PLEAzH5sDwKT1rV-UMfIGCrhp-PjNlAycj'
-directory = '/Users/atillasaadat/Music/iTunes/iTunes Media/Music/'
-folderName = 'youtube/'
+import argparse
+parser = argparse.ArgumentParser(description='get info')
+parser.add_argument('-d', type=str, help='music directory')
+parser.add_argument('-l', type=str, help='youtube playlist link')
+args = parser.parse_args()
+playlistLink = 'https://www.youtube.com/playlist?list=PLEAzH5sDwKT1rV-UMfIGCrhp-PjNlAycj' if args.l is None else args.l
+directory = '../' if args.d is None else args.d
+folderName = 'youtubeMusic/'
 folder = directory+folderName
 if not os.path.exists(folder):
     os.makedirs(folder)
